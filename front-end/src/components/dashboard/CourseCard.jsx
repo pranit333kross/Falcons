62be3b975bbfabbdce506f13a8a497ffcb5a3064
Pranit Biswal <pranitbiswal72@gmail.com>
2025-09-18 19:54:10 +0530

    CourseCard

--- front-end/src/components/dashboard/CourseCard.jsx
+++ front-end/src/components/dashboard/CourseCard.jsx
@@ -20,12 +20,12 @@ const CourseCard = ({ course, onSelect, index }) => {
             className={`bg-white rounded-xl shadow-lg overflow-hidden transform hover:-translate-y-2 hover:shadow-2xl transition-all duration-500 cursor-pointer group ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
                 }`}
         >
-            {/* <div className="p-6 bg-indigo-500 flex items-center justify-center h-32">
-                <BookOpen className="h-16 w-16 text-white group-hover:scale-110 transition-transform" />
-            </div> */}
-            <div className="p-6 bg-green-300 flex items-center justify-center h-32">
+            <div className="p-6 bg-indigo-500 flex items-center justify-center h-32">
                 <BookOpen className="h-16 w-16 text-white group-hover:scale-110 transition-transform" />
             </div>
+            {/* <div className="p-6 bg-green-300 flex items-center justify-center h-32">
+                <BookOpen className="h-16 w-16 text-white group-hover:scale-110 transition-transform" />
+            </div> */}
 
             <div className="p-6">
                 <h3 className="text-xl font-bold text-slate-800 mb-2">{course.title}</h3>
